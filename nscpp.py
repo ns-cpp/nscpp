a24a7b1a866b4308925ef78661e43cbcbcf575ab
nspya <enesceylan58@yahoo.com>
2019-12-03 16:12:51 +0300

    Create nscpp.py

---
+++ nscpp.py
@@ -0,0 +1,34 @@
+def nscpp():
+    print("""
+
+
+
+
+
+
+
+
+
+    nnnn  nnnnnnnn             ssssssssss                         cccccccccccccccc     ppppp   ppppppppp        ppppp   ppppppppp   
+    n:::nn::::::::nn         ss::::::::::s                      cc:::::::::::::::c     p::::ppp:::::::::p       p::::ppp:::::::::p  
+    n::::::::::::::nn      ss:::::::::::::s                    c:::::::::::::::::c     p:::::::::::::::::p      p:::::::::::::::::p 
+    nn:::::::::::::::n     s::::::ssss:::::s                  c:::::::cccccc:::::c     pp::::::ppppp::::::p     pp::::::ppppp::::::p
+      n:::::nnnn:::::n      s:::::s  ssssss                   c::::::c     ccccccc      p:::::p     p:::::p      p:::::p     p:::::p
+      n::::n    n::::n        s::::::s                        c:::::c                   p:::::p     p:::::p      p:::::p     p:::::p
+      n::::n    n::::n           s::::::s                     c:::::c                   p:::::p     p:::::p      p:::::p     p:::::p
+      n::::n    n::::n     ssssss   s:::::s                   c::::::c     ccccccc      p:::::p    p::::::p      p:::::p    p::::::p
+      n::::n    n::::n     s:::::ssss::::::s                  c:::::::cccccc:::::c      p:::::ppppp:::::::p      p:::::ppppp:::::::p
+      n::::n    n::::n     s::::::::::::::s       ......       c:::::::::::::::::c      p::::::::::::::::p       p::::::::::::::::p 
+      n::::n    n::::n      s:::::::::::ss        .::::.        cc:::::::::::::::c      p::::::::::::::pp        p::::::::::::::pp  
+      nnnnnn    nnnnnn       sssssssssss          ......          cccccccccccccccc      p::::::pppppppp          p::::::pppppppp    
+                                                                                        p:::::p                  p:::::p            
+                                                                                        p:::::p                  p:::::p            
+                                                                                       p:::::::p                p:::::::p           
+                                                                                       p:::::::p                p:::::::p           
+                                                                                       p:::::::p                p:::::::p           
+                                                                                       ppppppppp                ppppppppp           
+
+
+
+
+    """)
